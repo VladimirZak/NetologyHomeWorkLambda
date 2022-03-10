b86083c6bae4e8c317ded2be6f0812598800beb7
VladimirZak <v.v.zak@list.ru>
2022-03-10 20:18:46 +0800

    add comments

--- Task1/Main.java
+++ Task1/Main.java
@@ -6,7 +6,11 @@ public class Main {
 
         int a = calc.plus.apply(1, 2);
         int b = calc.minus.apply(1,1); // переменная b будет равна 0
-        int c = calc.divide.apply(a, b); // деление на ноль, будет ArithmeticException
+        int c = calc.divide.apply(a, b); // деление на ноль, будет ArithmeticException.
+                                        // Добавить проверку делителя на ноль в теле лямбда-выражения
+                                        // переменной divide класса Calculator, если делитель равен нулю,
+                                        // то результат деления будет равен нулю. Или проверять на исключения и
+                                        // выводить сообщение о запрете деления на ноль.
 
        // int d = calc.isPositive.test(b) ? calc.divide.apply(a, b): 0;
 
